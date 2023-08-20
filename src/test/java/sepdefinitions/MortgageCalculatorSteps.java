d4083127197091d343733c219e09f83960f9c9d2
Kafle <kaflepk@amazon.com>
2023-08-20 07:52:48 -0400

    added step definitions

---
+++ src/test/java/sepdefinitions/MortgageCalculatorSteps.java
@@ -0,0 +1,54 @@
+package sepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MortgageCalculatorSteps {
+	private WebDriver driver;
+	
+	@Given("I go to mortgag calculator website")
+	public void i_go_to_mortgag_calculator_website() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter mortgage amount {string}")
+	public void i_enter_mortgage_amount(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter interest rate {string}")
+	public void i_enter_interest_rate(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter amortization year {string} and month {string}")
+	public void i_enter_amortization_year_and_month(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I select start year {string} and month {string}")
+	public void i_select_start_year_and_month(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter interest term year {string} and month {string}")
+	public void i_enter_interest_term_year_and_month(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify results\"{double}\"")
+	public void i_verify_results(Double double1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
--- src/test/resources/features/mortgageCalculatorTest.feature
+++ src/test/resources/features/mortgageCalculatorTest.feature
@@ -1,12 +1,10 @@
 Feature: Mortgage Calculator Test
 
-Scenario: Buying a townhouse
-
-Given I go to mortgag calculator website
-When I enter mortgage amount "400000"
-And I enter interest rate "7"
-And I enter amortization year "29" and month "11"
-And I select start year "2024" and month "10"
-And I enter interest term year "30" and month "6"
-And module 10 2 hrs
-
+  Scenario: Buying a townhouse
+    Given I go to mortgag calculator website
+    When I enter mortgage amount "400000"
+    And I enter interest rate "7"
+    And I enter amortization year "29" and month "11"
+    And I select start year "2024" and month "10"
+    And I enter interest term year "30" and month "6"
+    Then I verify results"2530.14"
